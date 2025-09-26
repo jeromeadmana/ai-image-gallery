b8273461a9e8c7ffe074c65437b61037964ac530
Jerome <jerome.admana@gmail.com>
2025-09-27 06:47:47 +0800

    Adjustment on configurations and scripts

--- backend/index.js
+++ backend/index.js
@@ -41,6 +41,5 @@ app.use("/images", imageRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-  console.log(`📖 Swagger docs at http://localhost:${PORT}/api-docs`);
+  console.log(`Server successfully running`);
 });
