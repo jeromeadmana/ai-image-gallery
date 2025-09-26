1aed9e64b9f89d8d6c257c497fdd94b95ffb15a0
Jerome <jerome.admana@gmail.com>
2025-09-27 07:09:55 +0800

    Adjust filename case

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "./components/protectedRoute";
 
 function App() {
   return (
